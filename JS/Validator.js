2feb173082639cd10fbc5a041e41420272441a91
Desarrollo <08ath11@gmail.com>
2024-10-10 20:54:38 -0500

    Introcuccion al DOM y selectores de html

---
+++ JS/Validator.js
@@ -0,0 +1,16 @@
+// console.log(document.getElementById('fecha'))
+// console.log(document.getElementsByClassName('clase'))
+// console.log(document.getElementsByTagName('input'))
+
+// console.log(document.querySelector('input')) /*para etiquetas*/
+// console.log(document.querySelectorAll('input')) /*para todas las etiquetas*/
+// console.log(document.querySelector('#fecha'))/*para id*/
+// console.log(document.querySelector('.clase'))/*para clases*/
+// console.log(document.querySelectorAll('.clase')) /*para todas las clases*/
+
+// console.log(document.querySelector('from button'))/*para seleccionar el botón de form, cuando haya otros botones fuera*/
+// console.log(document.querySelector('from button'))/*para seleccionar el botón de form, cuando haya otros botones fuera y estén en otros formularios*/
+
+// usar variables para hacer las manipulaciones, siempre.
+let input = document.getElementById('fecha')
+console.log(input)
